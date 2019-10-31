d7c5f6f9600cb873accb0449c70cb2057048729b
Sanjana M Hathwar <hathwar.sanjana@gmail.com>
2019-10-31 16:35:26 +0530

    Type.js

--- src/actions/type.js
+++ src/actions/type.js
@@ -1,2 +1,8 @@
 export const GET_INVITED_AUCTIONS = "GET_INVITED_AUCTIONS";
 export const INVITED_AUCTIONS_FAIL = "INVITED_AUCTIONS_FAIL";
+export const GET_BIDDER_INFO = "GET_BIDDER_INFO";
+export const FAIL_BIDDER_INFO = "FAIL_BIDDER_INFO";
+export const GET_ITEM = "GET_ITEM";
+export const SAVE_BID = "SAVE_BID";
+export const SUBMIT_BID = "SUBMIT_BID";
+
